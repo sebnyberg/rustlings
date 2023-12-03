2ff73ceaa66e95b19a601a177f7135c30b0d46f9
Sebastian Nyberg <seb@spaced.local>
2023-12-03 12:58:09 +0100

    primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,16 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(x: i32) -> i32 {
+    if x == 35 {
+        70
+    } else if x == 40 {
+        80
+    } else if x == 41 {
+        41
+    } else {
+        65
+    }
 }
